d32282e60f88c1b174c6d6255b88bf7e7a637890
Ceiba-NelsonGarcia <nelson.garcia@ceiba.com.co>
2022-05-07 04:55:16 -0500

    Actualización archivo ciclo_for.py

--- MInTIC_Ciclo_1/cilo_for.py
+++ MInTIC_Ciclo_1/cilo_for.py
@@ -1,4 +1,10 @@
-'''
-    cyles for
-'''
+# Different format of the Cycle FOR #
+
+print("Count number from 1 to 4")
+for dix in range(1, 5):
+    print(dix)
+
+print("Count number from 0 to 4")
+for dix in range(5):
+    print(dix)
 
